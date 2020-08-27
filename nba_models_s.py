6b54b0b6b949f588b9ef8badae755517add41b28
Ubuntu <ubuntu@ip-172-31-25-137.ec2.internal>
2020-08-27 16:07:15 +0000

    ran new model

--- nba_models_s.py
+++ nba_models_s.py
@@ -93,16 +93,6 @@ params = {
 
 
 # search.best_estimator_
-# XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-#              colsample_bynode=1, colsample_bytree=0.9791636319740136,
-#              gamma=0.031653016218398666, gpu_id=-1, importance_type='gain',
-#              interaction_constraints='', learning_rate=0.032887910887540325,
-#              max_delta_step=0, max_depth=2, min_child_weight=1, missing=nan,
-#              monotone_constraints='()', n_estimators=169, n_jobs=0,
-#              num_parallel_tree=1, random_state=0, reg_alpha=0, reg_lambda=1,
-#              scale_pos_weight=1, subsample=0.6058651279281114,
-#              tree_method='exact', validate_parameters=1, verbosity=None)
-
 
 search = RandomizedSearchCV(xgb_model, param_distributions=params, random_state=26, n_iter=200, cv=3, verbose=1, n_jobs=-1, return_train_score=True)
 
@@ -112,6 +102,70 @@ xgb_pred= search.best_estimator_.predict(X_test_s)
 mean_squared_error(y_test_s, xgb_pred, squared=False)
 
 search.best_estimator_.save_model('001.model')
+
+bst = xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+             colsample_bynode=1, colsample_bytree=0.9791636319740136,
+             gamma=0.031653016218398666, gpu_id=-1, importance_type='gain',
+             interaction_constraints='', learning_rate=0.032887910887540325,
+             max_delta_step=0, max_depth=2, min_child_weight=1,
+             monotone_constraints='()', n_estimators=169, n_jobs=0,
+             num_parallel_tree=1, random_state=0, reg_alpha=0, reg_lambda=1,
+             scale_pos_weight=1, subsample=0.6058651279281114,
+             tree_method='exact', validate_parameters=1, verbosity=None)
+bst.fit(X_train_s,y_train_s)
+bst_pred = bst.predict(X_test_s)
+mean_squared_error(y_test_s, bst_pred, squared=False)
+
+import matplotlib.pyplot as plt
+
+
+#taking out TEAM categorical columns:
+train_season.iloc[:,:86]
+
+X_train_nc = train_season.iloc[:,:86].drop('GAME_TOTAL', axis = 1).to_numpy()
+y_train_nc = train_season['GAME_TOTAL'].to_numpy()
+X_test_nc = test_season.iloc[:,:86].drop('GAME_TOTAL', axis = 1).to_numpy()
+y_test_nc = test_season['GAME_TOTAL'].to_numpy()
+
+base_nc = xgb.XGBRegressor(objective="reg:linear", random_state=26,
+                            verbosity=2, n_estimators = 200,
+                            eta=0.05, gamma=1)
+base_nc.fit(X_train_nc, y_train_nc)
+base_pred = base_nc.predict(X_test_nc)
+mean_squared_error(y_test_s, base_pred, squared=False)
+#returns 20.199345503121535 so better than the base with all cat data, interesting
+
+
+
+
+
+from sklearn.model_selection import cross_val_score, GridSearchCV, RandomizedSearchCV
+from scipy.stats import uniform, randint
+xgb_model = xgb.XGBRegressor()
+params = {
+    "colsample_bytree": uniform(0.7, 0.3),
+    "gamma": uniform(0, 0.5),
+    "learning_rate": uniform(0.03, 0.3), # default 0.1 
+    "max_depth": randint(2, 6), # default 3
+    "n_estimators": randint(150, 200), # default 100
+    "subsample": uniform(0.6, 0.4)
+}
+
+
+# search.best_estimator_
+
+search_nc = RandomizedSearchCV(xgb_model, param_distributions=params, random_state=26, n_iter=200, cv=3, verbose=1, n_jobs=-1, return_train_score=True)
+
+search_nc.fit(X_train_nc, y_train_nc)
+
+xgb_pred_nc= search_nc.best_estimator_.predict(X_test_nc)
+mean_squared_error(y_test_s, xgb_pred_nc, squared=False)
+
+search.best_estimator_.save_model('001.model')
+
+
+
+
 
 
 
